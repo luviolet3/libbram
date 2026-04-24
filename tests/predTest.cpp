cb299cf31122e47e6bd266fad911dc12035e3317
BlakeKessler <blakekessler123@gmail.com>
2026-04-24 18:45:39 -0400

    add 10 new test files (9 original ones and the example proof from Aris)

---
+++ tests/predTest.cpp
@@ -0,0 +1,24 @@
+#include "parser.hpp"
+#include "rapidxml-1.13/rapidxml_utils.hpp"
+#include "utils/logger.hpp"
+
+#define TEST(testfile) \
+  do { try {                                                            \
+    rapidxml::file<> xmlFile(testfile);                                 \
+    lb::Parser::parse(xmlFile.data());                                  \
+  } catch (std::exception& e) {                                         \
+    lb::Utils::Logger::log(lb::Utils::Logger::Level::ERROR, e.what());  \
+  } } while (0)
+
+int main() {
+   TEST("tests/pred-proof-1.bram");
+   TEST("tests/pred-proof-2.bram");
+   TEST("tests/pred-proof-3.bram");
+   TEST("tests/pred-proof-4.bram");
+   TEST("tests/pred-proof-5.bram");
+   TEST("tests/pred-proof-6.bram");
+   TEST("tests/pred-proof-7.bram");
+   TEST("tests/pred-proof-8.bram");
+   TEST("tests/pred-proof-9.bram");
+   TEST("tests/resolution-example.bram");
+}
